f0886c867a7018706c6d047e187f258a909ded55
J <55432390+Milerej@users.noreply.github.com>
2025-04-16 17:33:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,32 +19,6 @@ def check_password():
 
     if "password_correct" not in st.session_state:
         # First run, show input for password.
-            st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.error("⚠️ Password incorrect")
-        return False
-    else:
-        # Password correct.
-        return Trudef check_password():
-    """Returns `True` if the user had the correct password."""
-
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == "Showmethemoney":
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        # First run, show input for password.
         st.text_input(
             "Password", type="password", on_change=password_entered, key="password"
         )
@@ -59,6 +33,8 @@ def check_password():
     else:
         # Password correct.
         return True
+
+
 
 if check_password():
     st.set_page_config(page_title="Interactive Interdependency Graph", layout="wide")
